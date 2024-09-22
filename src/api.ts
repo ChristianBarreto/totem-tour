a0d6c700573992bbbe255be2e383c02d5bbc9397
ChristianBarreto <christiannabasbarreto@gmail.com>
2024-09-22 17:18:15 -0300

    adjustments

--- src/api.ts
+++ src/api.ts
@@ -24,8 +24,6 @@ const baseUrl = process.env.NODE_ENV === 'production'
    ? 'https://totem-2id4w5fuzq-uc.a.run.app'
    : 'http://127.0.0.1:5001/totem-tour/us-central1/totem'
 
-console.log(process.env)
-
 export const getProducts = async (): Promise<Products | void> => {
   const data = axios.get<Products>(`${baseUrl}/products`)
     .then((res) => {
