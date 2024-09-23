3a6eb12fd0c15d97d65bb027e77ae9659d1e9797
ChristianBarreto <christiannabasbarreto@gmail.com>
2024-09-23 14:47:27 -0300

    allow cors

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,25 @@
-const functions = require('firebase-functions');
+/* eslint-disable */
+// const {logger} = require("firebase-functions");
+const {onRequest} = require("firebase-functions/v2/https");
+const {initializeApp} = require("firebase-admin/app");
 const {getFirestore} = require("firebase-admin/firestore");
+const express = require("express");
+import {Request, Response } from "express";
 
+const app = express();
+
+app.use(function(req: Request, res: Response, next: any) {
+  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
+
+initializeApp();
 const db = getFirestore();
 
-exports.products = functions.https.onRequest(async (req: Request, res: Response) => {
+app.get("/products", async (req: Request, res: Response) => {
   const snapshot = await db.collection("products").get();
   const data: any[] = [];
   snapshot.forEach((doc: any) => {
@@ -11,51 +27,18 @@ exports.products = functions.https.onRequest(async (req: Request, res: Response)
       data.push({id: doc.id, ...doc.data()});
     }});
   res.json(data);
-})
+});
 
-exports.cities = functions.https.onRequest((req, res) => {
-  const number = Math.round(Math.random() * 100)
-  res.send(number.toString())
-})
-
-
-
-
-
-// /* eslint-disable */
-// // const {logger} = require("firebase-functions");
-// const {onRequest} = require("firebase-functions/v2/https");
-// const {initializeApp} = require("firebase-admin/app");
-// const {getFirestore} = require("firebase-admin/firestore");
-// const express = require("express");
-// const app = express();
-
-// import {Request, Response} from "express";
+app.get("/cities", async (req: Request, res: Response) => {
+  const snapshot = await db.collection("cities").get();
+  const data: any[] = [];
+  snapshot.forEach((doc: any) => {
+    data.push({id: doc.id, ...doc.data()});
+  });
+  res.json(data);
+});
 
 
-// initializeApp();
-// const db = getFirestore();
-
-// app.get("/products", async (req: Request, res: Response) => {
-//   const snapshot = await db.collection("products").get();
-//   const data: any[] = [];
-//   snapshot.forEach((doc: any) => {
-//     if (doc.data().isAvailable) {
-//       data.push({id: doc.id, ...doc.data()});
-//     }});
-//   res.json(data);
-// });
-
-// app.get("/cities", async (req: Request, res: Response) => {
-//   const snapshot = await db.collection("cities").get();
-//   const data: any[] = [];
-//   snapshot.forEach((doc: any) => {
-//     data.push({id: doc.id, ...doc.data()});
-//   });
-//   res.json(data);
-// });
-
-
-// exports.totem = onRequest(app);
+exports.totem = onRequest(app);
 
 
