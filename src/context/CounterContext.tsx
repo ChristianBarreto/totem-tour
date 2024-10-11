2e8894a55a10b1486145054e7f5460bbd596e477
ChristianBarreto <christiannabasbarreto@gmail.com>
2024-10-11 18:37:41 -0300

    fix couter

--- src/context/CounterContext.tsx
+++ src/context/CounterContext.tsx
@@ -21,7 +21,6 @@ export function CounterProvider({
   const [counter, dispatch] = useReducer<Reducer<number, ACTIONTYPE>>(counterReducer, initialCounter);
 
   useEffect(() => {
-    console.log(counter)
     const timer = setInterval(() => {
       if (counter > 0) {
         if (pageCountDown(window.location.href)){
