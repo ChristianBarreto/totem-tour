a74c69e262f3e47880404bafbac54962627612f6
ChristianBarreto <christiannabasbarreto@gmail.com>
2024-11-03 17:37:47 -0300

    amend

--- functions/src/totemPing/index.ts
+++ functions/src/totemPing/index.ts
@@ -1,12 +1,12 @@
-import { Request, Response } from 'express';
+import { Request, Response } from "express";
 import { getDbItem, mergeDbItem } from "..";
 
 export const getTotemPingById = async (req: Request, res: Response) => {
   const resp = await getDbItem("totemPings", req.params.id);
   res.json(resp);
-}
+};
 
 export const setTotemPingById = async (req: Request, res: Response) => {
   const resp = await mergeDbItem("totemPings", req.params.id, req.body);
   res.json(resp);
-}
+};
--- functions/src/totems/index.ts
+++ functions/src/totems/index.ts
@@ -1,10 +1,10 @@
-import { Request, Response } from 'express';
+import { Request, Response } from "express";
 import { addDbItem, editDbItem, getDbItem, getDbItems } from "..";
 
 export const addTotemById = async (req: Request, res: Response) => {
   const resp = await addDbItem("totens", req.body);
   res.json(resp);
-}
+};
 
 export const getTotens = async (req: Request, res: Response) => {
   const totens = await getDbItems("totens");
@@ -15,19 +15,19 @@ export const getTotens = async (req: Request, res: Response) => {
       .then((ping) => {
         resp.push({
           ...totem,
-          lastPing: ping.lastPing
+          lastPing: ping.lastPing,
         });
       }).catch(() => {
         resp.push({
           ...totem,
-          lastPing: null
+          lastPing: null,
         });
       });
 
     resp.length === totens.length &&
       res.status(200).json(resp.sort((a, b) => a.nickName - b.nickName));
   });
-}
+};
 
 export const getTotemById = async (req: Request, res: Response) => {
   getDbItem("totens", req.params.id).then((totem) => {
@@ -43,9 +43,9 @@ export const getTotemById = async (req: Request, res: Response) => {
       });
     });
   });
-}
+};
 
 export const editTotemById = async (req: Request, res: Response) => {
   const resp = await editDbItem("totens", req.body.id, req.body);
   res.json(resp);
-}
+};
