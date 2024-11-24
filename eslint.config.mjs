93ad427057b1f994dbb9215842af94491a6556b4
ChristianBarreto <christiannabasbarreto@gmail.com>
2024-11-24 09:47:43 -0300

    CI=false to avoid warning like errors and improve eslint react vertion detection

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,6 +14,11 @@ export default [
     rules: {
       "react/jsx-uses-react": "off",
       "react/react-in-jsx-scope": "off"
+    },
+    settings: {
+      "react": {
+        "version": "detect"
+        }
     }
   },
   {
