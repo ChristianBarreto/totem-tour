0f03dd4076ca8322ba9b76298a039f104332088c
ChristianBarreto <christiannabasbarreto@gmail.com>
2024-12-05 15:16:05 -0300

    add delete to cors

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -51,7 +51,7 @@ app.use(function(request: Request, response: Response, next: any) {
   response.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
   response.setHeader("Access-Control-Allow-Origin", "*");
   response.setHeader("Access-Control-Allow-Credentials", "true");
-  response.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  response.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS,HEAD");
   response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
   next();
 });
