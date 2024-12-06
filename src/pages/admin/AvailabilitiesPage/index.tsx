949e8eb0111e772e7ccd281e59dc6c2b7aef1869
ChristianBarreto <christiannabasbarreto@gmail.com>
2024-12-05 22:16:41 -0300

    add loading to delete old availab. button

--- src/pages/admin/AvailabilitiesPage/index.tsx
+++ src/pages/admin/AvailabilitiesPage/index.tsx
@@ -6,10 +6,13 @@ import { Availabilities } from "../../../api/availabilities/types";
 
 export default function AvailabilitiesPage() {
   const [oldAvailabilities, setOldAvailabilities] = useState<Availabilities>([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const getOldAvailabilities = () => {
+    setIsLoading(true);
     getAvailabilities({date: {lt: {str: dayjs().locale('pt-br').format('YYYY-MM-DD')}}, orderBy: "date", order: "asc"}).then((res) => {
       setOldAvailabilities(res as Availabilities);
+      setIsLoading(false);
     }).catch((err) => {
       console.log("Err", err)
     });
@@ -20,8 +23,10 @@ export default function AvailabilitiesPage() {
   }, [])
 
   const handleDeleteOld = () => {
+    setIsLoading(true);
     deleteAvailabilities({date: {lt: {str: dayjs().locale('pt-br').format('YYYY-MM-DD')}}, orderBy: "date", order: "asc"}).then(() => {
       getOldAvailabilities();
+      setIsLoading(false);
     }).catch((err) => {
       console.log("Err", err)
     });
@@ -31,7 +36,13 @@ export default function AvailabilitiesPage() {
   return (
     <div>
       <p>Disponibilidades</p>
-      <button className="btn btn-sm bg-red-400 text-white" disabled={oldAvailabilities.length === 0} onClick={handleDeleteOld}>Delete old ({oldAvailabilities.length})</button>
+      <button
+        className="btn btn-sm bg-red-400 text-white"
+        disabled={(oldAvailabilities.length === 0) || isLoading}
+        onClick={handleDeleteOld}
+      >
+        {!isLoading ? `Deletar antigas (${oldAvailabilities.length})` : "Carregando..."}
+      </button>
       <AvailabilityTable filter={["!==", "isTest"]} sort="cityId" />
       <AvailabilityTable filter={["===", "isTest"]} sort="cityId" />
     </div>
